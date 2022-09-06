4f408e43a4a515e9d2f15c95f536bcb34339860e
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 12:14:43 +0300

    Add properties for Person

--- OOP_Home_Work/OOP/OOP/Person.cs
+++ OOP_Home_Work/OOP/OOP/Person.cs
@@ -13,5 +13,11 @@ namespace OOP
         private int age;
         private string phone;
 
+        internal string Name { get { return name; } set { name = value; } }
+
+        internal int Age { get { return age; } set { age = value>0?value:0; Console.WriteLine("Age must be grater than 0"); } }
+
+        public string Phone { get { return phone; } set { phone = value; } }
+
     }
 }
