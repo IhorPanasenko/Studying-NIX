cb00916f310646b410a68b05d78d8bb8d0e7e21b
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 15:51:21 +0300

    Edit SportGym

--- OOP_Home_Work/OOP/OOP/SportGym.cs
+++ OOP_Home_Work/OOP/OOP/SportGym.cs
@@ -3,13 +3,14 @@
     internal class SportGym
     {
         private Coach coach;
-        private Sportsman[] sportsmen;
+        private Group group;
         private string address;
         private double square;
 
         internal Coach Coach { get { return coach;} set { coach = value == null ? new Coach() : value; } }
-        public string Address { get { return address; } set { address = value; } }
-        public double Square { get { return square; } set { square = value>0?value:10; } }
+        internal string Address { get { return address; } set { address = value; } }
+        internal double Square { get { return square; } set { square = value>0?value:10; } }
+        internal Group Group { get { return group; } set { group = value; } }
 
         public SportGym()
         {
