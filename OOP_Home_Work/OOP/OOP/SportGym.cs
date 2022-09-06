752fa0249847e8e2ae4b7095c41f20778542d14a
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 16:00:16 +0300

    add constructors and methods

--- OOP_Home_Work/OOP/OOP/SportGym.cs
+++ OOP_Home_Work/OOP/OOP/SportGym.cs
@@ -7,14 +7,46 @@
         private string address;
         private double square;
 
-        internal Coach Coach { get { return coach;} set { coach = value == null ? new Coach() : value; } }
+        internal Coach Coach { get { return coach;} }
         internal string Address { get { return address; } set { address = value; } }
         internal double Square { get { return square; } set { square = value>0?value:10; } }
         internal Group Group { get { return group; } set { group = value; } }
 
         public SportGym()
         {
+            coach = new Coach();
+            Group = new Group();
+            Square = 10;
+            Address = "Somewhere on Earth";
+        }
 
+        public SportGym(Coach Coach, Group Group, string Address, double Square)
+        {
+            this.coach = Coach;
+            this.Group = Group;
+            this.Address = Address;
+            this.Square = Square;
+        }
+
+        public void ChangeCoach (Coach newCoach) 
+        {
+            this.coach = newCoach;
+        }
+
+        public void MoveInAnotherGym(int newSquare, string newAddress)
+        {
+            this.Address = newAddress;
+            this.Square = newSquare;
+        }
+
+        public bool RemoveSportsmanByName(string name)
+        {
+           return group.DeleteSportsmanByName(name);
+        }
+
+        public void AddSportsman(Sportsman sp)
+        {
+            group.AddSportsman(sp);
         }
 
     }
