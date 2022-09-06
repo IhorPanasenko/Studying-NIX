da2fe23f65f570eebcfe3f405501df1acfc65b14
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 15:38:47 +0300

    add fields, properties and empty constructor

--- OOP_Home_Work/OOP/OOP/SportGym.cs
+++ OOP_Home_Work/OOP/OOP/SportGym.cs
@@ -2,5 +2,19 @@
 {
     internal class SportGym
     {
+        private Coach coach;
+        private Sportsman[] sportsmen;
+        private string address;
+        private double square;
+
+        internal Coach Coach { get { return coach;} set { coach = value == null ? new Coach() : value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public double Square { get { return square; } set { square = value>0?value:10; } }
+
+        public SportGym()
+        {
+
+        }
+
     }
 }
