6112c4d5213b6e7713d4db018dcbbd5381ffe158
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 15:49:09 +0300

    add constructors and methods

--- OOP_Home_Work/OOP/OOP/Group.cs
+++ OOP_Home_Work/OOP/OOP/Group.cs
@@ -12,5 +12,48 @@ namespace OOP
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public Group()
+        {
+            sportsmen = new List<Sportsman>();
+            Id = 0;
+            Name = "Choose Name";
+        }
+
+        public Group (Sportsman sportsman, int id, string name)
+        {
+            this.sportsmen = new List<Sportsman>();
+            sportsmen.Add(sportsman);
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSportsman(Sportsman sportsman)
+        {
+            if (sportsman != null)
+            {
+                sportsmen.Add(sportsman);
+            }
+            else
+            {
+                Console.WriteLine("Sportsman must be someone");
+            }
+        }
+
+        public bool DeleteSportsmanByName(string name)
+        {
+            bool result = false;
+
+            foreach(Sportsman sp in sportsmen)
+            {
+                if(sp.Name == name)
+                {
+                    sportsmen.Remove(sp);
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
     }
 }
