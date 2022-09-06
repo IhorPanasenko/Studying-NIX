2a98ad4c8009918610c45969fca3fb3ed3d96e52
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 15:18:21 +0300

    Add constructors

--- OOP_Home_Work/OOP/OOP/sportsman.cs
+++ OOP_Home_Work/OOP/OOP/sportsman.cs
@@ -1,12 +1,29 @@
 ﻿namespace OOP
 {
-    internal class sportsman:Person
+    internal class Sportsman:Person
     {
         internal bool HasAbonement { get; set; }
         internal int WentToGymForMonths { get; set; }
         internal int WonCompetitions { get; set; }
-        internal Coach coach { get; set; }
+        internal Coach? Coach { get; set; }
 
+        public Sportsman():base()
+        {
+            HasAbonement = false;
+            WentToGymForMonths = 0;
+            WonCompetitions = 0;
+            Coach = null;
+        }
 
+        public Sportsman (Person per, bool HasAbonement, int WentToGymForMonths, int WonCompetitions, Coach? Coach)
+        {
+            this.Name = per.Name;  
+            this.Age = per.Age;
+            this.Phone = per.Phone;
+            this.HasAbonement = HasAbonement;
+            this.WentToGymForMonths = WentToGymForMonths;
+            this.WonCompetitions = WonCompetitions;
+            this.Coach = Coach;
+        }
     }
 }
