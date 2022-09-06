601c904f5ca2a72af6b5697fd1d900fa7543e4de
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 15:28:11 +0300

    Add ShowInfo method

--- OOP_Home_Work/OOP/OOP/sportsman.cs
+++ OOP_Home_Work/OOP/OOP/sportsman.cs
@@ -5,17 +5,17 @@
         internal bool HasAbonement { get; set; }
         internal int WentToGymForMonths { get; set; }
         internal int WonCompetitions { get; set; }
-        internal Coach? Coach { get; set; }
+        internal Coach Coach { get; set; }
 
         public Sportsman():base()
         {
             HasAbonement = false;
             WentToGymForMonths = 0;
             WonCompetitions = 0;
-            Coach = null;
+            Coach = new Coach();
         }
 
-        public Sportsman (Person per, bool HasAbonement, int WentToGymForMonths, int WonCompetitions, Coach? Coach)
+        public Sportsman (Person per, bool HasAbonement, int WentToGymForMonths, int WonCompetitions, Coach Coach)
         {
             this.Name = per.Name;  
             this.Age = per.Age;
@@ -36,6 +36,18 @@
             {
                 HasAbonement=true;
                 Console.WriteLine("Abonement was given to " + Name + "\n\n");
+            }
+        }
+
+        new public void ShowInfo()
+        {
+            if (IsMale)
+            {
+                Console.WriteLine(Name+" is sportsman he is"+Age+" years old \nHe has been gone to gym for "+WentToGymForMonths+" months and won "+WonCompetitions+" competitions\nHe training at coach "+Coach.Name+"\nHe has abonement "+HasAbonement+"Contact phone"+Phone);
+            }
+            else
+            {
+                Console.WriteLine(Name + " is sportsman she is" + Age + " years old \nShe has been gone to gym for " + WentToGymForMonths + " months and won " + WonCompetitions + " competitions\nShe training at coach " + Coach.Name + "\nShe has abonement " + HasAbonement + "Contact phone" + Phone);
             }
         }
 
