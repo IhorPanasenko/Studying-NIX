adb54afb9e587d7f23bfb9c8e53b370bc08384f0
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-06 14:55:55 +0300

    Add male field and propertie and additional changes

--- OOP_Home_Work/OOP/OOP/Person.cs
+++ OOP_Home_Work/OOP/OOP/Person.cs
@@ -12,12 +12,15 @@ namespace OOP
         private string name;
         private int age;
         private string phone;
+        private bool isMale;
 
         internal string Name { get { return name; } set { name = value; } }
 
-        internal int Age { get { return age; } set { age = value>0?value:0;); } }
+        internal int Age { get { return age; } set { age = value > 0 ? value : 0; } }
 
         public string Phone { get { return phone; } set { phone = value; } }
+
+        public bool IsMale { get { return isMale; } set { isMale = value; } }
 
         public Person()
         {
@@ -26,16 +29,17 @@ namespace OOP
             Phone = "Unknown";
         }
 
-        public Person(string name, int age, string phone)
+        public Person(string name, int age, string phone, bool isMale)
         {
             Name = name;
             Age = age;
             Phone = phone;
+            IsMale = isMale;
         }
 
         public void ShowInfo()
         {
-            Console.WriteLine("Name:" +Name+"\n"+"Age: "+Age+"\n"+"Phone: "+Phone+"\n\n");
+            Console.WriteLine("Name:" +Name+"\n"+"Age: "+Age+"\n"+"Phone: "+Phone+"\n"+"male: "+IsMale);
         }
 
     }
