a1f94f4ddf5e937d9ebd7aacedcc33be2dc4d59c
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-11 14:43:00 +0300

    Update RegisterUser.cs
    Start adding fields to RegisterUser

--- OOP_Final/VisualDisplay/E-ShopWithOOP/OOP_Hierarchy/RegisterUser.cs
+++ OOP_Final/VisualDisplay/E-ShopWithOOP/OOP_Hierarchy/RegisterUser.cs
@@ -2,11 +2,31 @@
 {
     public class RegisterUser
     {
-        private int id;
+        private readonly int id;
         private string firstName;
         private string lastName;
-        private DateTime BirthdayDate;
-        private readonly string password;
+        private DateTime birthdayDate;
+        private string password;
         private string login;
+        private string email;
+        private string phone;
+        private Address? defaultAddress;
+
+        internal int Id { get { return id; } }
+        internal string FirstName { get { return firstName; } set { FirstName = value != null ? value : firstName; } }
+        internal string LastName { get { return lastName; } set { LastName = value != null ? value : lastName; }}
+        internal DateTime BirthayDate { get { return birthdayDate; } set { BirthayDate = value != null ? value : birthdayDate; } }
+        public string Password { 
+            get 
+            {
+                return password; 
+            } 
+            set 
+            { 
+                
+            } 
+        }   
+        
+
     }
 }
